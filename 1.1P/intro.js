0a7b5004dd9bce058878b67295f404b3149b417e
Sarah Thomson <sthom120@gmail.com>
2026-07-06 15:04:53 +1000

    Add 1.1P setup files

---
+++ 1.1P/intro.js
@@ -0,0 +1,10 @@
+console.log("Welcome to SIT725");
+
+let studentName = "Sarah";
+let unit = "Applied Software Engineering";
+
+console.log("Student:", studentName);
+console.log("Unit:", unit);
+
+let weeklyGoal = "Practise JavaScript and Git";
+console.log("This week's goal is:", weeklyGoal);
